ccb7cd4f4ee4228c21a34c4a425fd3f8dbceec85
ccs568 <61998144+ccs568@users.noreply.github.com>
2025-11-04 16:57:21 +0800

    Remove UPnP and Full Cone NAT packages
    Removed UPnP and Full Cone NAT packages from the build.

--- mediatek-filogic/build24.sh
+++ mediatek-filogic/build24.sh
@@ -61,12 +61,6 @@ PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
 # 静态文件服务器dufs(推荐)
 PACKAGES="$PACKAGES luci-i18n-dufs-zh-cn
 
-# UPnP 功能（NAT-PMP / 端口自动映射）
-PACKAGES="$PACKAGES miniupnpd luci-app-upnp luci-i18n-upnp-zh-cn"
-
-# 全锥型 NAT（Full Cone NAT）
-PACKAGES="$PACKAGES kmod-nft-fullcone luci-app-fullconenat luci-i18n-fullconenat-zh-cn"
-
 # 第三方软件包 合并
 # ======== shell/custom-packages.sh =======
 if [ "$PROFILE" = "glinet_gl-axt1800" ] || [ "$PROFILE" = "glinet_gl-ax1800" ]; then
