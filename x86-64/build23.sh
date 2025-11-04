f5f833f8215d3192659c9f2fc7e238cda44f502c
ccs568 <61998144+ccs568@users.noreply.github.com>
2025-11-04 16:58:20 +0800

    Add UPnP and Full Cone NAT packages to build script

--- x86-64/build23.sh
+++ x86-64/build23.sh
@@ -59,6 +59,13 @@ PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
 # 静态文件服务器dufs(推荐)
 PACKAGES="$PACKAGES luci-i18n-dufs-zh-cn"
+
+# UPnP 功能（NAT-PMP / 端口自动映射）
+PACKAGES="$PACKAGES miniupnpd luci-app-upnp luci-i18n-upnp-zh-cn"
+
+# 全锥型 NAT（Full Cone NAT）
+PACKAGES="$PACKAGES kmod-nft-fullcone luci-app-fullconenat luci-i18n-fullconenat-zh-cn"
+
 # ======== shell/custom-packages.sh =======
 # 合并imm仓库以外的第三方插件
 PACKAGES="$PACKAGES $CUSTOM_PACKAGES"
