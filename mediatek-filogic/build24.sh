6bb886c0e51e6fd895f0eca17601a3fd11da4e49
ccs568 <61998144+ccs568@users.noreply.github.com>
2025-11-04 16:36:17 +0800

    Update build24.sh

--- mediatek-filogic/build24.sh
+++ mediatek-filogic/build24.sh
@@ -64,6 +64,9 @@ PACKAGES="$PACKAGES luci-i18n-dufs-zh-cn
 # UPnP 功能（NAT-PMP / 端口自动映射）
 PACKAGES="$PACKAGES miniupnpd luci-app-upnp luci-i18n-upnp-zh-cn"
 
+# 全锥型 NAT（Full Cone NAT）
+PACKAGES="$PACKAGES kmod-nft-fullcone luci-app-fullconenat luci-i18n-fullconenat-zh-cn"
+
 # 第三方软件包 合并
 # ======== shell/custom-packages.sh =======
 if [ "$PROFILE" = "glinet_gl-axt1800" ] || [ "$PROFILE" = "glinet_gl-ax1800" ]; then
